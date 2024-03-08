a6b4f2eef950d927a062092e0399fb510153da75
Alex Rodriguez <alex0807miami@gmail.com>
2024-03-08 18:12:15 -0500

    get data from db and display it

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    loadNotes();
+  }, []);
+
+  const loadNotes = async () => {
+    const result = await axios.get("http://localhost:8080/api/notes");
+    setNotes(result.data);
+  };
+
   return (
     <div className="container">
       <div className="py-4">
@@ -8,29 +20,19 @@ export default function Home() {
           <thead>
             <tr>
               <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">Content</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td colspan="2">Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
+            {notes.map((note, index) => (
+              <tr>
+                <th scope="row" key={index}>
+                  {index + 1}
+                </th>
+                <td>{note.content}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
