ae23de928bc9e3ca11e61921755ea113463fbb3f
Alex Rodriguez <alex0807miami@gmail.com>
2024-03-09 12:47:20 -0500

    add code to AddNote.js so that it saves note in database

--- src/notes/AddNote.js
+++ src/notes/AddNote.js
@@ -1,5 +1,56 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function AddNote() {
-  return <div>AddNote Form</div>;
+  let navigate = useNavigate();
+
+  const [note, setNote] = useState({
+    content: "",
+  });
+
+  const { content } = note;
+
+  const onInputChange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:8080/api/notes", note);
+    navigate("/");
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">Add note</h2>
+          <form onSubmit={(e) => onSubmit(e)}>
+            <div className="mb-3">
+              <label htmlFor="Name" className="form-label">
+                Content
+              </label>
+              <div style={{ marginBottom: "20px" }}>
+                <textarea
+                  className="form-control"
+                  style={{ height: "200px" }}
+                  placeholder="Type your note"
+                  name="content"
+                  value={content}
+                  onChange={(e) => onInputChange(e)}
+                />
+              </div>
+              <Link className="btn btn-outline-danger mx-2" to={"/"}>
+                Cancel
+              </Link>
+              <button type="submit" className="btn btn-outline-primary">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
