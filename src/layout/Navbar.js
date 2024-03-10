a275f571a7133c3af6b482360b5887c9186c8ad4
Alex Rodriguez <alex0807miami@gmail.com>
2024-03-09 19:24:33 -0500

    link Navbar to Home

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -6,9 +6,9 @@ export default function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <Link className="navbar-brand" to={"/"}>
             MyNotes
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
