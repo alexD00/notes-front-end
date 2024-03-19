5a095dce7d243ca4484469dd74aea3afcb04b90c
Alex Rodriguez <alex0807miami@gmail.com>
2024-03-19 10:27:20 -0400

    set all Routes as Protected except login

--- src/App.js
+++ src/App.js
@@ -12,18 +12,59 @@ import AddNote from "./notes/AddNote";
 import EditNote from "./notes/EditNote";
 import ViewNote from "./notes/ViewNote";
 import Login from "./pages/Login";
+import { Navigate } from "react-router-dom";
 
 function App() {
+  const ProtectedRoute = ({ children }) => {
+    const isAuthenticated = !!sessionStorage.getItem("jwtToken");
+
+    return isAuthenticated ? children : <Navigate to="/" />;
+  };
+
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Login />} />
-          <Route exact path="/home" element={<Home />} />
-          <Route exact path="/addNote" element={<AddNote />} />
-          <Route exact path="/editNote/:id" element={<EditNote />} />
-          <Route exact path="/viewNote/:id" element={<ViewNote />} />
+
+          {/* <Route exact path="/home" element={<Home />} /> */}
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/addNote"
+            element={
+              <ProtectedRoute>
+                <AddNote />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/editNote/:id"
+            element={
+              <ProtectedRoute>
+                <EditNote />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/viewNote/:id"
+            element={
+              <ProtectedRoute>
+                <ViewNote />
+              </ProtectedRoute>
+            }
+          />
+
           <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
