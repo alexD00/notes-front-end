401179144e4b3bf6c34d748738a5ef685ce569a0
Alex Rodriguez <alex0807miami@gmail.com>
2024-03-19 21:55:00 -0400

    add links to login and signup to PageNotSupported

--- src/pages/PageNotSupported.js
+++ src/pages/PageNotSupported.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function PageNotSupported() {
   return (
     <div>
-      <h1>Page Not Supported</h1>
-      <p>The page you are trying to access is not supported.</p>
+      <div>
+        <h1>Page Not Supported</h1>
+        <p>The page you are trying to access is not supported.</p>
+      </div>
+      <div></div>
+      <div style={{ marginTop: "60px" }}>
+        Have an account? <Link to="/">Log in</Link>
+      </div>
+      <div style={{ marginTop: "20px" }}>
+        Don't have an account? <Link to="/signUp">Sign up</Link>
+      </div>
     </div>
   );
 }
