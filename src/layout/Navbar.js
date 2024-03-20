c8d34732309c108b555a5e4075d3df67e276b7c8
Alex Rodriguez <alex0807miami@gmail.com>
2024-03-20 13:31:00 -0400

    add menu button to Navbar

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import { IoIosMenu } from "react-icons/io";
 
 export default function Navbar() {
   const location = useLocation();
@@ -12,6 +13,10 @@ export default function Navbar() {
   if (isLoginPage || isRootPath || isUnsupported || isSignUpPage) {
     return null;
   }
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("jwtToken");
+  };
 
   return (
     <div>
@@ -31,10 +36,43 @@ export default function Navbar() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-
           <Link className="btn btn-outline-light" to="/addNote">
             Add Note
           </Link>
+          <div className="nav-item dropdown">
+            <button
+              className="btn btn-outline-light dropdown-toggle"
+              type="button"
+              id="userDropdown"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <IoIosMenu />
+            </button>
+            <ul
+              className="dropdown-menu dropdown-menu-end"
+              aria-labelledby="userDropdown"
+            >
+              <li>
+                <Link className="dropdown-item" to="/profile">
+                  User Profile
+                </Link>
+              </li>
+              <li>
+                <hr class="dropdown-divider" />
+              </li>
+              <li>
+                <Link
+                  className="dropdown-item"
+                  to="/"
+                  style={{ color: "red" }}
+                  onClick={handleLogout}
+                >
+                  Log out
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </div>
